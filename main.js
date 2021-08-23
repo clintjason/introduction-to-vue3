d1b8c5d90a560d006453d72c94d4accf6f206499
CJ <ngassajason@yahoo.fr>
2021-08-23 11:45:46 +0100

    ✨ adds conditional Rendering

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const app = Vue.createApp({
     data: function() {
         return {
-            product: 'Jesus',
+            product: 'Socks',
             description: 'I love ',
             image: './assets/images/socks_blue.jpg',
-            url: 'https://mail.google.com/mail/u/0/#inbox'
+            inStock: false,
+            inventory: 0,
+            onSale: true
         }
     }
 })
