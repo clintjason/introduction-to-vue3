e5aaa977976ac9a6b19696bde00797e270addd24
CJ <ngassajason@yahoo.fr>
2021-08-23 11:51:08 +0100

    💄 updates URL

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const app = Vue.createApp({
             product: 'Jesus',
             description: 'I love ',
             image: './assets/images/socks_blue.jpg',
-            url: 'https://mail.google.com/mail/u/0/#inbox'
+            url: 'https://www.google.com/search?q=google+image&rlz=1C5CHFA_enCM904CM904&sxsrf=ALeKk02Fx-FKzvvlqGBadIdLz7RbKigRCw:1629715663336&source=lnms&tbm=isch&sa=X&sqi=2&ved=2ahUKEwiypsr5-8byAhUMQzABHQIEDTsQ_AUoAXoECAEQAw&biw=1440&bih=789#imgrc=JvPNEIlxszXQpM&imgdii=vT5ttlbN0IFXxM'
         }
     }
 })
