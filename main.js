4df0b13f354366e55fd5a918aee4dbbd4458896e
CJ <ngassajason@yahoo.fr>
2021-08-23 12:34:08 +0100

    ✨ Adds list rendering in vuejs

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 const app = Vue.createApp({
     data: function() {
         return {
-            product: 'Jesus',
+            product: 'Stock',
             description: 'I love ',
             image: './assets/images/socks_blue.jpg',
-            url: 'https://www.google.com/search?q=google+image&rlz=1C5CHFA_enCM904CM904&sxsrf=ALeKk02Fx-FKzvvlqGBadIdLz7RbKigRCw:1629715663336&source=lnms&tbm=isch&sa=X&sqi=2&ved=2ahUKEwiypsr5-8byAhUMQzABHQIEDTsQ_AUoAXoECAEQAw&biw=1440&bih=789#imgrc=JvPNEIlxszXQpM&imgdii=vT5ttlbN0IFXxM'
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            sizes: ['S', 'M', 'L', 'XL'],
+            variants: [
+              { id: 2234, color: 'green' },
+              { id: 2235, color: 'blue' },
+            ],
+            inStock: false
         }
     }
 })
